44454416ab10ac99ece4b75f4663a75a572fa0cc
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-04-26 00:46:13 +0700

    feat: Use middleware option

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("api/db.json");
 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ noCors: true });
 
 server.use(middlewares);
 server.use(
@@ -10,11 +10,6 @@ server.use(
     "/api/*": "/$1",
   })
 );
-server.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ecommerce-homepage.vercel.app/')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
 server.use(router);
 
 server.listen(3000, () => {
